87898fb72a0d6c72279cd74e3b3fd85d1edec35f
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-01 20:54:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+
+
+
 function getFormvalue() {
     //Write your code here
-
+	const fname=document.forms["form1"]["fname"].value;	
+	const lname=document.querySelector('[name="lname"]').value;
+	alert(fname+' '+lname);
 }
